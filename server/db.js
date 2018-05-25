2787c25230dc6ce5b25cf87e60766b4494f499d7
임운택 <wkblack11@gmail.com>
2018-05-25 23:52:09 +0900

    set up couchbase, mysql, express

---
+++ server/db.js
@@ -0,0 +1,66 @@
+import mysql from 'mysql'
+import couchbase from 'couchbase'
+import uuid from 'uuid/v1'
+
+class RDB {
+  constructor() {
+    this.mysqlPool = mysql.createPool({
+      host: 'localhost',
+      user: 'root',
+      password: 'a1234567',
+      database: 'graduate'
+    })
+  }
+
+  getConnection() {
+    return new Promise((resolve, reject) => {
+      this.mysqlPool.getConnection((err, connection) => {
+        if (err) reject(err)
+        resolve(connection)
+      })
+    })
+  }
+
+  async transaction() {
+    const connection = await this.getConnection()
+  }
+}
+
+class NoSQL {
+  constructor() {
+    this.couchbase = couchbase
+    this.url = 'couchbase://localhost/'
+    this.bucketName = 'sensor'
+    this.cluster = new this.couchbase.Cluster(this.url)
+    this.cluster.authenticate('untaek', 'a1234567')
+    this.bucket = this.cluster.openBucket(this.bucketName)
+    this.N1qlQuery = couchbase.N1qlQuery
+  }
+
+  init() {
+    const bucket = this.bucket
+  }
+
+  push({ deviceID, ownerID, sensorType, measuredValue }) {
+    const bucket = this.bucket
+    const id = uuid()
+    bucket.insert(
+      id, // key (uuid)
+      {
+        device_id: deviceID,
+        owner_id: ownerID,
+        sensor_id: sensorType,
+        measured_value: measuredValue,
+        time_stamp: new Date()
+      },
+      (err, result) => {
+        bucket.get(id, (err, result) => {
+          console.log(result)
+        })
+      }
+    )
+  }
+}
+
+export const rdb = new RDB()
+export const couch = new NoSQL()
--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import app from './app'
 import config from './config'
 
+import { couch } from './db'
+
 app.listen(config, () => {
   console.log('express is start')
+  /*for (let i = 0; i < 100000; i++) {
+    const doc = {
+      deviceID: Math.floor(Math.random() * 100),
+      ownerID: Math.floor(Math.random() * 100),
+      sensorType: 'dogweight',
+      measuredValue: (Math.random() * 5 + 2).toFixed(3)
+    }
+    couch.push(doc)
+  }*/
 })
